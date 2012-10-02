0eca80fc8b7b898bb2946c22309a1fbca449312e
Stuart Caborn <stooferus@gmail.com>
2012-10-02 20:25:27 +1300

    First happy path for tcp client

---
+++ src/RiemanClientSpike/TcpClient.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using ProtoBuf;
+using com.aphyr.riemann;
+
+namespace RiemanClientSpike
+{
+    public class RiemannTcpClient : IDisposable
+    {
+        private readonly TcpClient client;
+
+        public RiemannTcpClient(string host = "localhost", int port = 5555)
+        {
+            //TODO - add a connect method instead?
+            client = new TcpClient(host, port);
+        }
+
+        public void Dispose()
+        {
+            client.Close();
+        }
+
+        public void SendEvent(string host = null, float? metric = null, string service = null, string state = "ok", string description = null)
+        {
+            var stateEntry = new State
+                                 {
+                                     Host = host,
+                                     Service = service,
+                                     state = state,
+                                     metric_f = metric,
+                                     Description = description
+                                 };
+
+            var message = new Msg{states =new[]{stateEntry}};
+            var tcpStream = client.GetStream();
+            Serializer.SerializeWithLengthPrefix(tcpStream, message, PrefixStyle.Fixed32BigEndian);
+            
+            //TODO - test cases for sad path.
+            var result = Serializer.DeserializeWithLengthPrefix<Msg>(tcpStream, PrefixStyle.Fixed32BigEndian);
+        }
+
+        public IEnumerable<Event> Query(string query)
+        {
+            var message = new Msg
+                              {
+                                  states = new []{new State{Description = "test query", Service = "tests", state = "info"}, },
+                                  query = new Query{QueryString = query}
+                              };
+
+            var tcpStream = client.GetStream();
+            Serializer.SerializeWithLengthPrefix(tcpStream, message, PrefixStyle.Fixed32BigEndian);
+            var result = Serializer.DeserializeWithLengthPrefix<Msg>(tcpStream, PrefixStyle.Fixed32BigEndian);
+
+            return result.events ?? new Event[0];
+        }
+    }
+}
---
+++ src/RiemannClientTests/TcpClientTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RiemanClientSpike;
+using com.aphyr.riemann;
+
+namespace RiemannClientTests
+{
+    [TestFixture]
+    public class TcpClientTest
+    {
+        [Test]
+        public void Publishing_state_is_written_to_riemann_index()
+        {
+            var serviceName = "TcpClientTest " + Guid.NewGuid();
+            var description = Guid.NewGuid().ToString();
+            var metric = DateTime.Now.Ticks;
+            
+//            (use 'riemann.client)
+//            (def c (tcp-client :host "1.2.3.4"))
+//            (send-event c {:service "foo" :state "ok"})
+//            (query c "state = \"ok\"")
+
+            IEnumerable<Event> results;
+            
+            using (var client = new RiemannTcpClient(host: "127.0.0.1", port: 5555))
+            {
+                client.SendEvent(host: "tests",
+                                 service: serviceName,
+                                 state: "ok",
+                                 description: description,
+                                 metric: metric);
+
+                results = client.Query(string.Format("service=\"{0}\"", serviceName)).ToList();
+
+            }
+            Assert.That(results.Any());
+            Assert.That(results.Single().Description, Is.EqualTo(description));
+            Assert.That(results.Single().Service, Is.EqualTo(serviceName));
+            Assert.That(results.Single().State, Is.EqualTo("ok"));
+            Assert.That(results.Single().metric_f, Is.EqualTo(metric));
+        }
+    }
+}
